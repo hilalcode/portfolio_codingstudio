f6fcae7a633d69fd204db6c35de757e1079b26dd
hilalcode <hilalbow011@gmail.com>
2024-02-11 22:29:30 +0700

    add about

--- main.js
+++ main.js
@@ -8,8 +8,6 @@ function toggleDetail(e) {
         $(target).html("Less Info").addClass("active")
     }
 
-
-
     const item = $(target).parents('.about-exp-item')
     const detail = $(item).children(".about-exp-item-detail")
 
